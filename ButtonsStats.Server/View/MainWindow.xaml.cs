9d6b6a686b9171d345f2946a716acfb5bf1b46aa
Kopchigashev-Ilya <ilya.kopchigashev@gmail.com>
2023-02-13 11:36:20 +0300

    Add project files.

---
+++ ButtonsStats.Server/View/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows;
+
+namespace ButtonsStats.Server
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
